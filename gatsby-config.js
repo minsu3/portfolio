a68844d649d4225fd08bd32eca762a975005b48e
Min Su Kim <kim97minsu@gmail.com>
2021-01-27 16:40:05 -0800

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: "Portfolio - Minsu Kim",
   },
-  plugins: ["gatsby-plugin-offline"],
+  plugins: [
+    "gatsby-plugin-offline",
+    `gatsby-plugin-styled-components`, // I AM NEW
+  ],
 };
